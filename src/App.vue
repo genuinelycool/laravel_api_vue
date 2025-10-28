f7a9a27f559c765ea5a8c6e8ddb2e5a73019dd52
genuinelycool <genuinelycool@gmail.com>
2025-10-28 12:49:33 +0545

    add nav-link class to App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/" class="nav-link">Home</RouterLink>
     </nav>
   </header>
 
