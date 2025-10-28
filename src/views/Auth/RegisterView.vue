be795d6871b23384b53b9d697439d99f8acfdb59
genuinelycool <genuinelycool@gmail.com>
2025-10-28 19:21:50 +0545

    form for register

--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,9 +1,40 @@
 <script setup>
+import { reactive } from 'vue';
+
+const formData = reactive({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: ""
+});
 
 </script>
 
 <template>
-  <main>
-    <h1 class="title">Register a new account</h1>
-  </main>
+    <main>
+        <h1 class="title">Register a new account</h1>
+
+        <form 
+            @submit.prevent="console.log(formData)" 
+            class="w-1/2 mx-auto space-y-6"
+        >
+            <div>
+                <input type="text" placeholder="Name" v-model="formData.name" />
+            </div>
+
+            <div>
+                <input type="text" placeholder="Email" v-model="formData.email" />
+            </div>
+
+            <div>
+                <input type="password" placeholder="Password" v-model="formData.password" />
+            </div>
+
+            <div>
+                <input type="password" placeholder="Confirm Password" v-model="formData.password_confirmation" />
+            </div>
+
+            <button class="primary-btn">Register</button>
+        </form>
+    </main>
 </template>
